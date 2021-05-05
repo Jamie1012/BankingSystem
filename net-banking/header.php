50d92d3c312c24da0781863c99d3624559eeaf2e
Jasmine <jasminnie1012@gmail.com>
2021-05-05 16:48:08 +0530

    LoginPage Changed

---
+++ net-banking/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header_style.css">
+</head>
+<style media="screen">
+
+</style>
+<body>
+    <div style="background-color: black; height:70px" class="flex-container-header">
+        <div class="flex-item-header">
+            <img src="images/cicon.png" onclick="eEgg_func()" width="50" height="50">
+        </div>
+        <div  class="flex-item-header">
+            <h1 style="color:#f5f5f5";>PREMIER BANK</h1>
+        </div>
+    </div>
+
+</body>
+</html>
